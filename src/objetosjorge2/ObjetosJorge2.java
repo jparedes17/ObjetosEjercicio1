44b639554066485bf8d2a65689c0440feb9ccdf6
jparedes17 <paredesjorge@hotmail.com>
2016-09-06 11:22:56 -0500

    Interfaz Grafica TERMINADA

---
+++ src/objetosjorge2/ObjetosJorge2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package objetosjorge2;
+
+/**
+ *
+ * @author jparedes2
+ */
+public class ObjetosJorge2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
